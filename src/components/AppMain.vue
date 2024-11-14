defa0ab025bcb22647737a77f42d1fbcace7a574
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-14 17:27:30 +0100

    added v-for to show projects data on AppMain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -14,7 +14,6 @@ export default {
             axios.get(this.ApiProjectUrl)
                 .then(response => {
                     this.projectList = response.data.response;
-                    console.log(response.data.response);
                 })
 
                 .catch(function (error) {
@@ -40,20 +39,24 @@ export default {
             </div>
 
             <div class="col-12">
-                <div class="card bg-light mb-4 shadow">
+                <div class="card bg-light mb-4 shadow" v-for="project in projectList" key="project.id">
                     <div class="card-header bg-primary text-white">
-                        <h5></h5>
+                        <h5>{{project.title }}</h5>
                     </div>
                     <div class="card-image text-center p-2">
                         <img src="" alt="" class=" rounded">
                     </div>
                     <div class="card-body">
-                        <p><strong>Description: {{projectList[1].description }}</strong></p>
-                        <p><strong>URL:</strong></p>
-                        <p><strong>Start Date:</strong></p>
-                        <p><strong>End Date:</strong></p>
-                        <p><strong>Type Name:</strong></p>
-                        <p><strong>Type Description:</strong></p>
+                        <p><strong>Author:</strong> {{project.name }}</p>
+                        <p><strong>Description:</strong> {{project.description }}</p>
+                        <p><strong>URL:</strong> {{project.url }}</p>
+                        <p><strong>Start Date:</strong> {{project.start_date }}</p>
+                        <p><strong>End Date:</strong> {{project.end_date }}</p>
+                        <p><strong>Type Name:</strong> {{project.type.name }}</p>
+                        <p><strong>Type Description:</strong> {{project.type.description }}></p>
+                        <p><strong>Technologies:</strong></p> 
+                        <p v-for="(technology,index) in project.technologies">
+                            {{technology.name }}</p>
                     </div>
                 </div>
             </div>
