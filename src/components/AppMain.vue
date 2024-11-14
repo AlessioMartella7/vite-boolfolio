8867d5a3da11b873539a955b2c3698f082080ca3
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-14 17:47:41 +0100

    added AppMainCard components and gave it props projectListObject

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,13 @@
 <script>
 import axios from 'axios';
+import AppMainCard from './AppMainCard.vue';
 
 export default {
+    components:{
+        AppMainCard,
+    },
     name: 'AppMain',
+
     data() {
         return {
             projectList:[],
@@ -37,29 +42,9 @@ export default {
             <div class="col-12 my-4">
                 <h1 class="fw-bold text-center" >Projects List</h1>
             </div>
-
-            <div class="col-12">
-                <div class="card bg-light mb-4 shadow" v-for="project in projectList" key="project.id">
-                    <div class="card-header bg-primary text-white">
-                        <h5>{{project.title }}</h5>
-                    </div>
-                    <div class="card-image text-center p-2">
-                        <img src="" alt="" class=" rounded">
-                    </div>
-                    <div class="card-body">
-                        <p><strong>Author:</strong> {{project.name }}</p>
-                        <p><strong>Description:</strong> {{project.description }}</p>
-                        <p><strong>URL:</strong> {{project.url }}</p>
-                        <p><strong>Start Date:</strong> {{project.start_date }}</p>
-                        <p><strong>End Date:</strong> {{project.end_date }}</p>
-                        <p><strong>Type Name:</strong> {{project.type.name }}</p>
-                        <p><strong>Type Description:</strong> {{project.type.description }}></p>
-                        <p><strong>Technologies:</strong></p> 
-                        <p v-for="(technology,index) in project.technologies">
-                            {{technology.name }}</p>
-                    </div>
-                </div>
-            </div>
+            <AppMainCard
+            :projectListObject = 'projectList'
+            />
         </div>
     </div>
 </main>
