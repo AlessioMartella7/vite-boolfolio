d642c8bc6f0a45296ddf6d008a980da3237bacff
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-14 17:51:05 +0100

    exercise complete with bonus

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -24,7 +24,6 @@ export default {
                 .catch(function (error) {
                   console.log(error);
                 });
-              
         },
     },
 
@@ -38,7 +37,6 @@ export default {
 <main>
     <div class="container">
         <div class="row">
-
             <div class="col-12 my-4">
                 <h1 class="fw-bold text-center" >Projects List</h1>
             </div>
@@ -51,4 +49,5 @@ export default {
 </template>
 
 <style scoped>
+
 </style>
