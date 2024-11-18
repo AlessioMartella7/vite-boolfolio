67a8c008fc3f961b4b8b5b3f3991626ac07cc428
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-18 17:16:42 +0100

    fixed apiUrl in AppSingleProject

--- src/pages/AppSingleProject.vue
+++ src/pages/AppSingleProject.vue
@@ -7,14 +7,14 @@ export default {
  
     data() {
         return {
-        $apiUrl: 'http://127.0.0.1:8000/api/projects',
-        $singleProject: {},
+        apiUrl: 'http://127.0.0.1:8000/api/projects',
+        singleProject: {},
       
     }
     },
     methods:{
         getSingleProject(){
-            axios.get(`${this.$apiUrl}/${this.$route.params.project}`)
+            axios.get(`${this.apiUrl}/${this.$route.params.project}`)
                 .then(response => {
                     console.log('ciao')
                     console.log(response);
