40f2f30b16d609f29c8401f82dbc6974049bc8ce
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-18 16:42:33 +0100

    added AppSingleProject.vue page

--- src/js/router.js
+++ src/js/router.js
@@ -3,11 +3,13 @@ import { createWebHistory, createRouter } from 'vue-router'
 import AppHome from '../pages/AppHome.vue'
 import AppProjects from '../pages/AppProjects.vue'
 import AppTechnologies from '../pages/AppTechnologies.vue'
+import AppSingleProject from '../pages/AppSingleProject.vue'
 
 const routes = [
   { path: '/', component: AppHome, name: 'home' },
   { path: '/projects', component: AppProjects, name: 'projects'},
   { path: '/technologies', component: AppTechnologies, name: 'technologies' },
+  { path: '/projects/:project', component: AppSingleProject, name: 'project' },
 ]
 
 const router = createRouter({
---
+++ src/pages/AppSingleProject.vue
@@ -0,0 +1,43 @@
+<script>
+import axios from 'axios';
+import AppProjectCard from '../components/AppProjectCard.vue';
+export default {
+    name: 'AppSingleProject',
+    components: AppProjectCard,
+ 
+    data() {
+        return {
+        $apiUrl: 'http://127.0.0.1:8000/api/projects',
+        $singleProject: {},
+      
+    }
+    },
+    methods:{
+        getSingleProject(){
+            axios.get(`${this.$apiUrl}/${this.$route.params.project}`)
+                .then(response => {
+                    console.log('ciao')
+                    console.log(response);
+                    this.$singleProject = response.data.response;
+                })
+
+                .catch(function (error) {
+                  console.log(error);
+                });
+        },
+    },
+    created(){
+        this.getSingleProject();
+    }
+}
+</script>
+
+<template>
+    <h1>Project</h1>
+    <AppProjectCard :singleProjectObject = '$singleProject'
+    
+    />
+</template>
+
+<style scoped>
+</style>
